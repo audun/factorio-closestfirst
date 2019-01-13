01a6db39934f5fca530dcfaa7a6277bab3e6dc3b
Audun Ellertsen <audune@gmail.com>
2019-01-13 20:09:42 +0100

    Fix build_zip mkdir option

--- build_zip.sh
+++ build_zip.sh
@@ -2,7 +2,7 @@
 
 VERSION=0.1.3
 
-mkdir -f ClosestFirst_$VERSION
+mkdir -p ClosestFirst_$VERSION
 cp -prv info.json *.lua locale *.txt *.md ClosestFirst_$VERSION/
 zip -r ClosestFirst_$VERSION.zip ClosestFirst_$VERSION
 cp ClosestFirst_$VERSION.zip ~/Library/Application\ Support/factorio/mods/
