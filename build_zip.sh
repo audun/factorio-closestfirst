de57a3f999998a9c0e344dec7a9bb9823c26cbf3
Audun Ellertsen <audune@gmail.com>
2019-02-28 19:05:35 +0100

    Added thumbnail.png

--- build_zip.sh
+++ build_zip.sh
@@ -3,7 +3,8 @@
 VERSION=`grep \"version\" info.json | cut -f2 -d: | sed 's/[\", ]//g'`
 
 mkdir -p ClosestFirst_$VERSION
-cp -prv info.json *.lua locale *.txt *.md ClosestFirst_$VERSION/
+cp -prv info.json *.lua locale *.txt *.md thumbnail.png ClosestFirst_$VERSION/
+rm -f ClosestFirst_$VERSION.zip
 zip -r ClosestFirst_$VERSION.zip ClosestFirst_$VERSION
 cp ClosestFirst_$VERSION.zip ~/Library/Application\ Support/factorio/mods/
 
