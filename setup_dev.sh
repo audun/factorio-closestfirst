81973526c40e2a1dfd92ed7b0268940c1edb6d50
Audun Ellertsen <audune@gmail.com>
2019-03-07 00:06:51 +0100

    Quick dev setup script

---
+++ setup_dev.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+VERSION=`grep \"version\" info.json | cut -f2 -d: | sed 's/[\", ]//g'`
+NAME=ClosestFirst_$VERSION
+ZIP=$NAME.zip
+
+mkdir -p 
+cp -prv info.json *.lua locale *.txt *.md thumbnail.png $NAME/
+rm -f $ZIP
+zip -r $ZIP $NAME
+cp $ZIP ~/Library/Application\ Support/factorio/mods/
+cd ~/Library/Application\ Support/factorio/mods/
+unzip -o $ZIP
