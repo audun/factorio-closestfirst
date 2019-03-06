8b070ea8ddf5fe1367e880d4d04f4e77cf2a3395
Audun Ellertsen <audune@gmail.com>
2019-03-06 23:43:20 +0100

    Fix typo in defines, add 10x10 and update_rate table + formatting

--- defines.lua
+++ defines.lua
@@ -4,17 +4,27 @@ return {
    update_rate_setting = "italics-closestfirst-update_rate_setting",
    limit_area_setting = "italics-closestfirst-limit_area_setting",
    range_setting_table = {
-      ["20x20"] = 20, 
-      ["30x30 (Roboport Mk1)"] = 30,
-      ["40x40 (Roboport Mk2)"] = 40,
-      ["50x50"] = 50,
-      ["60x60 (4x Roboport Mk1)"] = 60,
-      ["70x70"] = 70,
-      ["80x80 (4x Roboport Mk2)"] = 80,
-      ["90x90"] = 90,
+      ["No limit (up to equipped Roboport range)"] = 0,
+      ["10x10"]                     =  10,
+      ["20x20"]                     =  20,
+      ["30x30 (Roboport Mk1)"]      =  30,
+      ["40x40 (Roboport Mk2)"]      =  40,
+      ["50x50"]                     =  50,
+      ["60x60 (4x Roboport Mk1)"]   =  60,
+      ["70x70"]                     =  70,
+      ["80x80 (4x Roboport Mk2)"]   =  80,
+      ["90x90"]                     =  90,
       ["100x100 (6x Roboport Mk2)"] = 100,
-      ["120×120 (9x Roboport Mk2)"] = 110,
-      ["No limit (up to equipped Roboport range)"] = 0
+      ["120×120 (9x Roboport Mk2)"] = 120
+   },
+   update_rate_setting_table = {
+      ["Off"]     =   0,
+      ["Fastest"] =   2,
+      ["Faster"]  =   8,
+      ["Fast"]    =  16,
+      ["Normal"]  =  32,
+      ["Slow"]    =  64,
+      ["Slower"]  =  96,
+      ["Slowest"] = 128
    }
 }
-
